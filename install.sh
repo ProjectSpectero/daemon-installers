2bc2f5806e8068e58b0f3377e4e1369502b3f567
Andrew Haughie <elycin2@gmail.com>
2018-05-09 20:49:03 -0400

    Removing documentation from script and referencing the repository for guidance

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 ###############################################################################
 ##
 ##  Spectero, Inc
-##  Copyright (c) 2018 - All rights reserved 
+##  Copyright (c) 2018 - All rights reserved
+##  https://spectero.com
 ## 
 ##  By using this script you are agreeing to the terms of services
 ##  provided to the end user at https://spectero.com/tos
+##
+##  For command line arguments, please refer to the readme in the GitHub Repo
+##  https://github.com/ProjectSpectero/daemon-installer-nix
 ##
 ###############################################################################
 
@@ -139,55 +143,6 @@ import sys
 import zipfile
 import traceback
 import urllib.request
-
-"""
-Spectero Installer
-Copyright (c) 2018 Spectero - All Rights Reserved
-https://spectero.com/
-
-Description:
-    This utility serves the purpose to install the daemon and command line tools into your operating system
-    It supports Mac OS X and Linux variants.
-
-Warranty:
-    This software is provided without warranty.
-
-Liability:
-    The user runs this application at their own risk, and takes responsibility for any system changes.
-    You will be promoted to read the terms service upon execution and installation.
-
-Command Line Arguments:
-
-    Release Channels:
-        Release channels are categorized based on how stable the builds are.
-
-        --alpha
-            Use the alpha channel and get bleeding edge builds of the daemon.
-            It is ill-advised to use this channel as bugs may be present.
-
-        --beta
-            Use the beta channel and get semi-stable builds that contain new features.
-            This channel also may contain bugs.
-
-        --stable
-            Use the stable channel and get release candidates that are deemed suitable for production
-            (this flag is used by default)
-
-    Other:
-        --agree
-            Agree to the Spectero Terms of Service located at https://spectero.com/tos.
-
-        --install-dotnet
-            Automatically install the .NET Core Framework if it does not exist.
-
-        --no-prompt
-            Disable all Spectero related prompts and automatically install into /opt/spectero
-
-        --overwrite
-            Force an overwrite of the current version installed.
-            If this is not provided, the installer will assume it should check for an update.
-
-"""
 
 INSTALLER_VERSION = 1
 
