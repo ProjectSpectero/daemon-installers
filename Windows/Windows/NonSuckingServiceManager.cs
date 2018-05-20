fe74e1798a0c45dbddd02c4dd51e942cb8688dbd
Ely Haughie <elycin2@gmail.com>
2018-05-19 20:20:26 -0400

    Started NSSM template

---
+++ Windows/Windows/NonSuckingServiceManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace installer
+{
+    class NonSuckingServiceManager
+    {
+        public static bool Exists(string InstallLocation)
+        {
+            return "";
+        }
+
+        public static void Delete()
+        {
+        }
+
+        public static void Create()
+        {
+        }
+
+        public static void Stop()
+        {
+        }
+
+        public static void Start()
+        {
+        }
+    }
+}
