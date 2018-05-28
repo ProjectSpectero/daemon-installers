82acc473b9c780785ff8766fd7f09ab083338784
Ely Haughie <Elycin2@gmail.com>
2018-05-28 07:00:30 -0400

    Refactoring DotNetCore.cs to support dynamic download link selection

--- Windows/Windows/DotNetCore.cs
+++ Windows/Windows/DotNetCore.cs
@@ -5,12 +5,19 @@ namespace installer
 {
     internal class DotNetCore
     {
-        public static string PortableRuntimeDownloadLink = "https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-win-x64.zip";
+        // 32 bit download link for dotnet core
+        public static string X86DownloadLink = 
+            "https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-win-x64.zip";
+        
+        // 64 bit download link for dotnet core
+        public static string X64DownloadLink = 
+            "https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-win-x64.zip";
 
+        // List of potential paths where dotnet core can be installed in the system.
         private static readonly string[] DotnetPotentialPaths =
         {
-            "C:\\Program Files\\dotnet\\dotnet.exe",
-            "C:\\Program Files (x86)\\dotnet\\dotnet.exe",
+            "C:\\Program Files (x86)\\dotnet\\dotnet.exe", // 32 bit path
+            "C:\\Program Files\\dotnet\\dotnet.exe" // 64 bit path
         };
 
         public static bool Exists() => DotnetPotentialPaths.Any(File.Exists);
@@ -18,14 +25,12 @@ namespace installer
         public static string GetDotnetPath()
         {
             foreach (var dotnetPotentialPath in DotnetPotentialPaths)
-            {
                 if (File.Exists(dotnetPotentialPath))
-                {
                     return dotnetPotentialPath;
-                }
-            }
 
             return null;
         }
+        
+        public static string GetDownloadLinkFromArch() => () ? X64DownloadLink : X86DownloadLink 
     }
 }
