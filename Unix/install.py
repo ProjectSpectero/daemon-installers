9dfc5977aa27282df41cba6b790390a05a47001c
Ely Haughie <elycin2@gmail.com>
2018-07-16 18:38:10 -0400

    Appending username to usermod, confirming -d -m valid flags.

--- Unix/install.py
+++ Unix/install.py
@@ -173,7 +173,7 @@ def fix_permissions():
     print("Fixing directory ownership...")
     os.system("chown -R spectero:spectero %s" % get_install_directory_from_config())
     os.system("chmod -R 744 %s" % get_install_directory_from_config())
-    os.system("usermod -m -d %s" % get_install_directory_from_config()[:-1])
+    os.system("usermod -m -d %s spectero" % get_install_directory_from_config()[:-1])
 
 
 def get_dotnet_core_path():
