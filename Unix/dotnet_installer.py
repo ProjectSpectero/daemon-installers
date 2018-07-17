483b26a9ad6408ffd382f976a1e22d78014ef6d6
Ely Haughie <elycin2@gmail.com>
2018-07-17 07:46:16 -0400

    reconfiguring sources.json and creating skeleton for dotnet_installer

---
+++ Unix/dotnet_installer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+###############################################################################
+##
+##  Spectero, Inc
+##  Copyright (c) 2018 - All rights reserved
+##  https://spectero.com
+##
+##  This python script is a special case, and should not be used without
+##  the proper 'install.sh'.
+##
+##  The bash script produces a configuration that this script is designed
+##  to read. Although you can write your own configuration if you
+##  study this script, it is not advised.
+##
+##  This script is also subject to Spectero's Terms of Service.
+##  You may find this document at https://spectero.com/tos
+##
+###############################################################################
+
+import json
+import urllib.request
+
