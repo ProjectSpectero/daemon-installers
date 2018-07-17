af60e87be2585f5426f32c25ce642a665d2b499b
Ely Haughie <elycin2@gmail.com>
2018-07-17 07:52:45 -0400

    Adding config reading and exception

--- Unix/dotnet_installer.py
+++ Unix/dotnet_installer.py
@@ -19,5 +19,53 @@
 ###############################################################################
 
 import json
+import sys
+import os
 import urllib.request
 
+config = {}
+sources = {}
+
+
+def exception_config_missing():
+    print("The installer requires a proper configuration file.")
+    print("Please make sure you ran the install.sh")
+    print("If this issue still exists, please report it to the developers.")
+    sys.exit(1)
+
+
+def download_sources():
+    global sources
+    request = urllib.request.Request('https://raw.githubusercontent.com/ProjectSpectero/daemon-installers/master/SOURCES.json')
+    result = urllib.request.urlopen(request)
+    sources = json.loads(result.read().decode('utf-8'))
+
+
+def read_config():
+    global config
+
+    # Check if install instructions exist
+    if not os.path.isfile("/tmp/spectero.installconfig"):
+        exception_config_missing()
+
+    # Read the install instructions
+    with open("/tmp/spectero.installconfig", 'rU') as f:
+        for line_terminated in f:
+            line = line_terminated.rstrip('\n')
+            splitline = line.split('=')
+            key = splitline[0]
+            value = None
+            if len(splitline) > 2:
+                splitline = splitline.pop(0)
+                value = splitline.join("=")
+
+            elif len(splitline) == 2:
+                value = splitline[1]
+
+            # Update the dictionary
+            config[key] = value
+
+
+if __name__ == "__main__":
+    read_config()
+    download_sources()
